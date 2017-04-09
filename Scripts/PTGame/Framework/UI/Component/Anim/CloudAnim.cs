444f44a6bacb9adb0600634a8773f0996584b0d4
SnowCold <snowcold.develop@gmail.com>
2017-04-09 18:27:34 +0800

    添加动画组件

---
+++ Scripts/PTGame/Framework/UI/Component/Anim/CloudAnim.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using PTGame.Framework;
+
+namespace PTGame.Framework
+{
+    public class CloudAnim : MonoBehaviour
+    {
+        [SerializeField]
+        private Rect m_Rect;
+        [SerializeField]
+        private float m_SpeedX = 10;
+
+        private float m_R = 5;
+        private float m_BaseY;
+        private float m_CurrentSpeed;
+
+        private Vector3 m_OriPosition;
+
+        private void Awake()
+        {
+            m_OriPosition = transform.localPosition;
+            RandomPath();
+        }
+
+        private void RandomPath()
+        {
+            //确定速度方向
+            int dir = RandomHelper.Range(0, 10);
+            int speed = (int)RandomHelper.Range(30, m_SpeedX);
+            m_BaseY = m_OriPosition.y + RandomHelper.Range(10, 100);
+
+            if (dir > 5)
+            {
+                transform.localPosition = new Vector3(m_Rect.xMax, m_BaseY, 0);
+                m_CurrentSpeed = -speed;
+            }
+            else
+            {
+                transform.localPosition = new Vector3(m_Rect.xMin, m_BaseY, 0);
+                m_CurrentSpeed = speed;
+            }
+
+            m_R = RandomHelper.Range(15, m_Rect.height);
+        }
+
+        private void Update()
+        {
+            Vector3 currentPos = transform.localPosition;
+            currentPos.x += m_CurrentSpeed * Time.deltaTime;
+            currentPos.y = m_BaseY + Mathf.Sin(currentPos.x * 0.03f) * m_R;
+
+            transform.localPosition = currentPos;
+
+            if (currentPos.x > m_Rect.xMax || currentPos.x < m_Rect.xMin)
+            {
+                RandomPath();
+            }
+        }
+    }
+}
---
+++ Scripts/PTGame/Framework/UI/Component/Anim/StarAnim.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using PTGame.Framework;
+using UnityEngine.UI;
+using DG.Tweening;
+
+namespace PTGame.Framework
+{
+    public class StarAnim : MonoBehaviour
+    {
+        [SerializeField]
+        private Image[] m_Stars;
+
+        private void Awake()
+        {
+            if (m_Stars.Length == 0)
+            {
+                m_Stars = transform.GetComponentsInChildren<Image>();
+            }
+
+            for (int i = 0; i < m_Stars.Length; ++i)
+            {
+                DOTween.Sequence().Append(m_Stars[i].DOFade(0, RandomHelper.Range(1.0f, 2.0f)))
+                    .Append(m_Stars[i].DOFade(1, RandomHelper.Range(0.5f, 2.0f)))
+                    .SetDelay(RandomHelper.Range(1.0f, 4.0f))
+                    .SetLoops(-1);
+            }
+        }
+    }
+}
