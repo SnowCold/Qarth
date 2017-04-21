84703f4fc816436e85d7be0a89473e8d05913876
ouyanggongping <ouyanggongping@putao.com>
2017-04-21 19:36:21 +0800

    添加分辨率适配帮助类

---
+++ Scripts/PTGame/Engine/UI/Helper/ResolutionHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PTGame.Framework
+{
+    [TMonoSingletonAttribute("[Tools]/ResolttionHelper")]
+    public class ResolutionHelper : TMonoSingleton<ResolutionHelper>
+    {
+        [SerializeField]
+        private float m_StandardCanvasScale;
+        [SerializeField]
+        private float m_CurrentCanvasScale;
+        [SerializeField]
+        private bool m_IsInStandardMode = true;
+
+        public override void OnSingletonInit()
+        {
+            Canvas uiCanvas = UIMgr.S.uiRoot.rootCanvas;
+            Camera uiCamera = UIMgr.S.uiRoot.uiCamera;
+            CanvasScaler canvasScaler = uiCanvas.GetComponent<CanvasScaler>();
+            m_StandardCanvasScale = uiCamera.orthographicSize / canvasScaler.referenceResolution.y * 2;
+
+            m_CurrentCanvasScale = uiCanvas.transform.localScale.y;
+
+            float offset = m_StandardCanvasScale - m_CurrentCanvasScale;
+            if (offset < 0.0001 && offset > -0.0001)
+            {
+                m_IsInStandardMode = true;
+            }
+            else
+            {
+                m_IsInStandardMode = false;
+            }
+        }
+
+        public Vector3 TranslateScale(Vector3 standardScale)
+        {
+            if (m_IsInStandardMode)
+            {
+                return standardScale;
+            }
+
+            standardScale.y = m_StandardCanvasScale * standardScale.y / m_CurrentCanvasScale;
+
+            return standardScale;
+        }
+    }
+}
