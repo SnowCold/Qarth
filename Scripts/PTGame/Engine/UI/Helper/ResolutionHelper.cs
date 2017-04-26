c2a7136e365ef3f3b8e506663d94905cd3197202
ouyanggongping <ouyanggongping@putao.com>
2017-04-26 19:30:54 +0800

    分辨率适配工具测试

--- Scripts/PTGame/Engine/UI/Helper/ResolutionHelper.cs
+++ Scripts/PTGame/Engine/UI/Helper/ResolutionHelper.cs
@@ -14,16 +14,31 @@ namespace PTGame.Framework
         [SerializeField]
         private RectTransform m_CanvasRoot;
 
+        private float m_StandardCanvasScale;
+        
         public override void OnSingletonInit()
         {
+            Camera uiCamera = UIMgr.S.uiRoot.uiCamera;
+            Canvas uiCanvas = UIMgr.S.uiRoot.rootCanvas;
             m_CanvasScaler = UIMgr.S.uiRoot.rootCanvas.GetComponent<CanvasScaler>();
+            m_StandardCanvasScale = uiCamera.orthographicSize / m_CanvasScaler.referenceResolution.y * 2;
+
             m_CanvasRoot = m_CanvasScaler.GetComponent<RectTransform>();   
         }
 
         public Vector3 TranslateScale(Vector3 standardScale)
         {
-            float scaleX = standardScale.x * m_CanvasRoot.rect.width / m_CanvasScaler.referenceResolution.x * 2;
-            float scaleY = standardScale.y * m_CanvasRoot.rect.height / m_CanvasScaler.referenceResolution.y * 2;
+            //面板的缩放比例
+            float nX = m_CanvasRoot.rect.width / m_CanvasScaler.referenceResolution.x;
+            float nY = m_CanvasRoot.rect.height / m_CanvasScaler.referenceResolution.y;
+
+            float scaleX = standardScale.x * (nX * m_StandardCanvasScale) / m_CanvasRoot.localScale.x;
+            float scaleY = standardScale.y * (nY * m_StandardCanvasScale) / m_CanvasRoot.localScale.x;
+            //float scaleX = standardScale.x * m_CanvasRoot.rect.width * m_CanvasRoot.localScale.x / (m_CanvasScaler.referenceResolution.x * m_StandardCanvasScale);
+            //float scaleY = standardScale.y * m_CanvasRoot.rect.height * m_CanvasRoot.localScale.y / (m_CanvasScaler.referenceResolution.y * m_StandardCanvasScale);
+
+            //float scaleX = m_StandardCanvasScale / m_CanvasRoot.localScale.x;
+            //float scaleY = m_StandardCanvasScale / m_CanvasRoot.localScale.y;
 
             return new Vector3(scaleX, scaleY, 1);
         }
