fef0ca5344fd28ff051b3baec7b889a2d733d042
snowcold <snowcold.ouyang@gmail.com>
2017-05-18 19:26:00 +0800

    修正文件服务器路径

--- Scripts/PTGame/Engine/ResSystem/ResUpdate/ResUpdateConfig.cs
+++ Scripts/PTGame/Engine/ResSystem/ResUpdate/ResUpdateConfig.cs
@@ -9,7 +9,7 @@ namespace PTGame.Framework
     {
         //public static string WEB_URL = http://localhost:8080/ptupdate/pailogic/res/new/{0}
 
-        public static string RES_WEB_URL = "http://static-storeptdev.cn/source/game_pailogic_ios/version1/res/";
-        public static string ZIP_WEB_URL = "http://static-storeptdev.cn/source/game_pailogic_ios/version1/res/";
+		public static string RES_WEB_URL = "http://static-store.ptdev.cn/source/game_pailogic_ios/version1/res/";
+		public static string ZIP_WEB_URL = "http://static-store.ptdev.cn/source/game_pailogic_ios/version1/res/";
     }
 }
