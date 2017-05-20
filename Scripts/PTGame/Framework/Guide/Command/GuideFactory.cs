b2fabec05a11cbb5f520131e091e714d572bca6f
SnowCold <snowcold.develop@gmail.com>
2017-05-20 17:54:56 +0800

    移除非framework文件

--- Scripts/PTGame/Framework/Guide/Command/GuideFactory.cs
+++ Scripts/PTGame/Framework/Guide/Command/GuideFactory.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using PTGame.Framework;
-
+/*
 namespace PTGame.PaiLogic
 {
     public class GuideFactory
@@ -57,3 +57,4 @@ namespace PTGame.PaiLogic
 
     }
 }
+*/
