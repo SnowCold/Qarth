5f69f1971c9bef2459bdc191ed87d217c1fd67c5
ouyanggongping <ouyanggongping@putao.com>
2017-06-02 16:44:34 +0800

    添加转场进度

---
+++ Scripts/PTGame/Framework/UI/Component/StepProgressBar/ProgressPositionSetter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PTGame.Framework
+{
+    public class ProgressPositionSetter : MonoBehaviour
+    {
+        [SerializeField, Range(0, 1)]
+        private float m_Progress;
+        [SerializeField]
+        private Transform m_Target;
+        [SerializeField]
+        private Vector3 m_StartLocalPosition;
+        [SerializeField]
+        private Vector3 m_EndLocalPosition;
+
+        public float progress
+        {
+            get { return m_Progress; }
+            set
+            {
+                m_Progress = value;
+                UpdateUI();
+            }
+        }
+
+        private void UpdateUI()
+        {
+            if (m_Target == null)
+            {
+                return;
+            }
+
+            Vector3 dis = m_EndLocalPosition - m_StartLocalPosition;
+            dis = dis * m_Progress;
+            Vector3 pos = m_StartLocalPosition + dis;
+            m_Target.localPosition = pos;
+        }
+    }
+}
---
+++ Scripts/PTGame/Framework/UI/Transition/ITransitionProgroess.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PTGame.Framework;
+
+namespace PTGame.PaiLogic
+{
+    public interface ITransitionProgress
+    {
+        float GetTransitionProgress();
+    }
+}
