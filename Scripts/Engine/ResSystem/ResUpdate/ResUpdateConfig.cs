d7f146c7483163b6f8a99a06a42355f99fccadd6
ouyanggongping <ouyanggongping@putao.com>
2017-06-16 17:33:15 +0800

    删除资源热更默认路径

--- Scripts/Engine/ResSystem/ResUpdate/ResUpdateConfig.cs
+++ Scripts/Engine/ResSystem/ResUpdate/ResUpdateConfig.cs
@@ -13,9 +13,7 @@ namespace Qarth
 {
     public class ResUpdateConfig
     {
-        //public static string WEB_URL = http://localhost:8080/ptupdate/pailogic/res/new/{0}
-
-		public static string RES_WEB_URL = "http://static-store.ptdev.cn/source/game_pailogic_ios/version1/res/";
-		public static string ZIP_WEB_URL = "http://static-store.ptdev.cn/source/game_pailogic_ios/version1/res/";
+		public static string RES_WEB_URL = "";
+		public static string ZIP_WEB_URL = "";
     }
 }
