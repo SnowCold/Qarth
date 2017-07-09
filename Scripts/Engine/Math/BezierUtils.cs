e0d93a913660062c3b8a660e9f6c99bd414fbea2
SnowCold <snowcold.develop@gmail.com>
2017-07-09 21:42:59 +0800

    更新

--- Scripts/Engine/Math/BezierUtils.cs
+++ Scripts/Engine/Math/BezierUtils.cs
@@ -13,7 +13,7 @@ namespace Qarth
 {
     public class BezierUtils
     {
-        public static Vector3 CalculateCubicBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2)
+        public static Vector3 Calculate2OrderPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2)
         {
             float u = 1 - t;
             float tt = t * t;
@@ -26,28 +26,70 @@ namespace Qarth
             return p;
         }
 
-        public static Vector3[] GetBeizerList(Vector3 startPoint, Vector3 controlPoint, Vector3 endPoint, int segmentNum)
+        public static Vector3[] Get2OrderPoints(Vector3 p0, Vector3 p1, Vector3 p2, int segmentNum)
         {
             Vector3[] path = new Vector3[segmentNum];
             for (int i = 1; i <= segmentNum; i++)
             {
                 float t = i / (float)segmentNum;
-                Vector3 pixel = CalculateCubicBezierPoint(t, startPoint,
-                    controlPoint, endPoint);
+                Vector3 pixel = Calculate2OrderPoint(t, p0,
+                    p1, p2);
                 path[i - 1] = pixel;
             }
             return path;
 
         }
 
-        public static void GetBeizerList(Vector3[] outData, Vector3 startPoint, Vector3 controlPoint, Vector3 endPoint)
+        public static void Get2OrderPoints(Vector3 p0, Vector3 p1, Vector3 p2, Vector3[] outData)
         {
             int segmentNum = outData.Length;
             for (int i = 1; i <= segmentNum; i++)
             {
                 float t = i / (float)segmentNum;
-                Vector3 pixel = CalculateCubicBezierPoint(t, startPoint,
-                    controlPoint, endPoint);
+                Vector3 pixel = Calculate2OrderPoint(t, p0,
+                    p1, p2);
+                outData[i - 1] = pixel;
+            }
+        }
+
+        public static Vector3 Calculate3OrderPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
+        {
+            float u = 1 - t;
+            float tt = t * t;
+            float uu = u * u;
+            float uuu = uu * u;
+            float ttt = tt * t;
+
+            Vector3 p = p0 * uuu;
+            p += 3 * p1 * t * uu;
+            p += 3 * p2 * tt * u;
+            p += p3 * ttt;
+
+            return p;
+        }
+
+        public static Vector3[] Get3OrderPoints(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, int segmentNum)
+        {
+            Vector3[] path = new Vector3[segmentNum];
+            for (int i = 1; i <= segmentNum; i++)
+            {
+                float t = i / (float)segmentNum;
+                Vector3 pixel = Calculate3OrderPoint(t, p0,
+                    p1, p2, p3);
+                path[i - 1] = pixel;
+            }
+            return path;
+
+        }
+
+        public static void Get3OrderPoints(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, Vector3[] outData)
+        {
+            int segmentNum = outData.Length;
+            for (int i = 1; i <= segmentNum; i++)
+            {
+                float t = i / (float)segmentNum;
+                Vector3 pixel = Calculate3OrderPoint(t, p0,
+                    p1, p2, p3);
                 outData[i - 1] = pixel;
             }
         }
