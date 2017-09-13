e7af412a145f24ff446474bae887330e3dc865f5
ouyanggongping <ouyanggongping@putao.com>
2017-09-13 11:04:45 +0800

    修改ResFactory实现

--- Scripts/Engine/ResSystem/Core/ResFactory.cs
+++ Scripts/Engine/ResSystem/Core/ResFactory.cs
@@ -14,7 +14,65 @@ namespace Qarth
 {
     public class ResFactory
     {
-        private delegate IRes ResCreator(string name);
+        public delegate IRes ResCreator(string name);
+
+        public interface IResCreatorWrap
+        {
+            bool CheckResType(string name);
+            IRes CreateRes(string name);
+        }
+
+        class ResCreatorWrap : IResCreatorWrap
+        {
+            private string m_Key;
+            private ResCreator m_Creator;
+
+            public ResCreatorWrap(string key, ResCreator creator)
+            {
+                m_Key = key;
+                m_Creator = creator;
+            }
+
+            public bool CheckResType(string name)
+            {
+                return name.StartsWith(m_Key);
+            }
+
+            public IRes CreateRes(string name)
+            {
+                return m_Creator(name);
+            }
+        }
+
+        class AssetResCreatorWrap : IResCreatorWrap
+        {
+            public IRes CreateRes(string name)
+            {
+                AssetData data = AssetDataTable.S.GetAssetData(name);
+
+                switch (data.assetType)
+                {
+                    case eResType.kAssetBundle:
+                        return AssetBundleRes.Allocate(name);
+                    case eResType.kABAsset:
+                        return AssetRes.Allocate(name);
+                    case eResType.kABScene:
+                        return SceneRes.Allocate(name);
+                    default:
+                        return null;
+                }
+            }
+
+            public bool CheckResType(string name)
+            {
+                AssetData data = AssetDataTable.S.GetAssetData(name);
+
+                return data != null;
+            }
+        }
+
+        private static List<IResCreatorWrap> s_CreatorList;
+        private static IResCreatorWrap s_AssetResCreatorWrap;
 
         static ResFactory()
         {
@@ -23,38 +81,56 @@ namespace Qarth
             ObjectPool<AssetRes>.S.maxCacheCount = 40;
             ObjectPool<InternalRes>.S.maxCacheCount = 40;
             ObjectPool<NetImageRes>.S.maxCacheCount = 20;
+
+            s_CreatorList = new List<IResCreatorWrap>();
+
+            s_AssetResCreatorWrap = new AssetResCreatorWrap();
+
+            RegisterResCreate("Resources/", InternalRes.Allocate);
+            RegisterResCreate("NetImage:", NetImageRes.Allocate);
+            RegisterResCreate("HotUpdateRes:", HotUpdateRes.Allocate);
+        }
+
+        public static void RegisterResCreate(string key, ResCreator creator)
+        {
+            if (creator == null || string.IsNullOrEmpty(key))
+            {
+                Log.e("Register InValid Creator.");
+                return;
+            }
+
+            RegisterResCreateWarp(new ResCreatorWrap(key, creator));
+        }
+
+        public static void RegisterResCreateWarp(IResCreatorWrap wrap)
+        {
+            if (wrap == null)
+            {
+                Log.e("Register InValid Wrap.");
+                return;
+            }
+
+            s_CreatorList.Add(wrap);
         }
 
         public static IRes Create(string name)
         {
-            short assetType = 0;
-            if (name.StartsWith("Resources/"))
+            if (s_AssetResCreatorWrap.CheckResType(name))
             {
-                assetType = eResType.kInternal;
-            }
-            else if (name.StartsWith("NetImage:"))
-            {
-                assetType = eResType.kNetImageRes;
-            }
-            else if (name.StartsWith("HotUpdateRes:"))
-            {
-                assetType = eResType.kHotUpdateRes;
+                return s_AssetResCreatorWrap.CreateRes(name);
             }
             else
             {
-                AssetData data = AssetDataTable.S.GetAssetData(name);
-                if (data == null)
+                for (int i = s_CreatorList.Count - 1; i >= 0; --i)
                 {
-                    Log.e("Failed to Create Res. Not Find AssetData:" + name);
-                    return null;
-                }
-                else
-                {
-                    assetType = data.assetType;
+                    if (s_CreatorList[i].CheckResType(name))
+                    {
+                        return s_CreatorList[i].CreateRes(name);
+                    }
                 }
             }
-
-            return Create(name, assetType);
+            Log.e("Not Find ResCreate For Res:" + name);
+            return null;
         }
 
         public static IRes Create(string name, short assetType)
