36acb9b7b743de7bcd0786f4dc5d16b8b3a15742
SnowCold <snowcold.develop@gmail.com>
2017-10-12 17:12:38 +0800

    修复sprite 可能丢失的问题

--- Scripts/Framework/UI/Component/Anim/SpriteAnim.cs
+++ Scripts/Framework/UI/Component/Anim/SpriteAnim.cs
@@ -12,12 +12,15 @@ namespace Qarth
         [SerializeField]
         private Image m_TargetImage;
         [SerializeField]
-        private Sprite[] m_Sprites;
+        private String[] m_SpritesNames;
         [SerializeField]
         private float m_Duration = 1.0f;
 
+        private Sprite[] m_SpritesArray;
         private int m_TimerID;
         private int m_CurrentIndex = 0;
+
+        private ResLoader m_ResLoader;
 
         private void Awake()
         {
@@ -26,18 +29,30 @@ namespace Qarth
                 m_TargetImage = GetComponent<Image>();
             }
 
-            if (m_TargetImage == null || m_Sprites == null || m_Sprites.Length == 0)
+            if (m_TargetImage == null || m_SpritesNames == null || m_SpritesNames.Length == 0)
             {
                 return;
             }
 
+            m_ResLoader = ResLoader.Allocate();
+            m_ResLoader.Add2Load(m_SpritesNames);
+
+            m_ResLoader.LoadSync();
+            m_SpritesArray = new Sprite[m_SpritesNames.Length];
+
+            for (int i = 0; i < m_SpritesNames.Length; ++i)
+            {
+                m_SpritesArray[i] = ResMgr.S.GetAsset<Sprite>(m_SpritesNames[i]);
+            }
+
+            OnTimeReach(0);
             m_TimerID = Timer.S.Post2Scale(OnTimeReach, m_Duration, -1);
         }
 
         private void OnTimeReach(int count)
         {
-            m_CurrentIndex = (++m_CurrentIndex) % m_Sprites.Length;
-            m_TargetImage.sprite = m_Sprites[m_CurrentIndex];
+            m_CurrentIndex = (++m_CurrentIndex) % m_SpritesArray.Length;
+            m_TargetImage.sprite = m_SpritesArray[m_CurrentIndex];
         }
 
         private void OnDestroy()
