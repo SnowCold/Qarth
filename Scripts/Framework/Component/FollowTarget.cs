2a1045f5c90a274f1cb8be135f51c41f39b34165
SnowCold <snowcold.develop@gmail.com>
2017-10-13 20:43:49 +0800

    添加跟随组件

---
+++ Scripts/Framework/Component/FollowTarget.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Qarth;
+
+namespace Qarth
+{
+    public class FollowTarget : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform m_Target;
+        [SerializeField]
+        private Vector3 m_Offset;
+        [SerializeField]
+        private Vector3 m_Axis = new Vector3(1,1,1);
+
+        private void LateUpdate()
+        {
+            if (m_Target == null)
+            {
+                return;
+            }
+
+            Vector3 pos = m_Target.position;
+            transform.position =  new Vector3(pos.x * m_Axis.x, pos.y * m_Axis.y, pos.z * m_Axis.z) + m_Offset;
+        }
+    }
+}
